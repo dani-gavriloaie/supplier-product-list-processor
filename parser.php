24bb077ed765abbdc4d2fb1f78c99d7b787635a6
Daniel Gavriloaie <dani.gavriloaie@gmail.com>
2025-03-03 17:36:28 +0000

    clean up entry point

--- parser.php
+++ parser.php
@@ -1,6 +1,5 @@
 <?php
 
-use DaniGavriloaie\SupplierProductListProcessor\Parsers\CliArgumentParser;
 use DaniGavriloaie\SupplierProductListProcessor\Services\ProductProcessor;
 
 require 'vendor/autoload.php';
@@ -11,16 +10,5 @@ if (empty($options['file']) || empty($options['unique-combinations'])) {
     die("Usage: php parser.php --file <file> --unique-combinations <output_file>\n");
 }
 
-$cliArgumentsParser = new CliArgumentParser($argv);
-$args = $cliArgumentsParser->getAll();
-
-if (!$args['file']) {
-    throw new Exception('\'file\' argument is missing');
-}
-
-if (!$args['unique-combinations']) {
-    throw new Exception('\'unique-combinations\' argument is missing');
-}
-
 $productProcessor = new ProductProcessor();
-$productProcessor->process();
+$productProcessor->process($options['file'], $options['unique-combinations']);
