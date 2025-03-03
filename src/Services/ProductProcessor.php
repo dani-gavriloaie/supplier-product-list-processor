b3e43dd7f466e7a2013dab928ee67228ab7595f2
Daniel Gavriloaie <dani.gavriloaie@gmail.com>
2025-03-03 20:16:06 +0000

    product processor prototype

--- src/Services/ProductProcessor.php
+++ src/Services/ProductProcessor.php
@@ -2,9 +2,38 @@
 
 namespace DaniGavriloaie\SupplierProductListProcessor\Services;
 
+use DaniGavriloaie\SupplierProductListProcessor\Exceptions\RequiredFieldException;
+use DaniGavriloaie\SupplierProductListProcessor\Factories\FileParserFactory;
+use DaniGavriloaie\SupplierProductListProcessor\Models\Product;
+
 class ProductProcessor
 {
-    public function process(): void
+    /** @throws RequiredFieldException|\Exception */
+    public function process(string $filePath, string $outputPath): void
     {
+        $fileParser = FileParserFactory::createParser($filePath);
+        $fileParser->parse($filePath);
+
+        /** @var Product $product */
+        $uniqueProducts = [];
+        foreach ($fileParser->parse($filePath) as $product) {
+            $hash = md5($product->toString());
+
+            if (in_array($hash, $uniqueProducts)) {
+                $uniqueProducts[$hash]['count'] += 1;
+            } else {
+                $uniqueProducts[$hash] = [
+                    'product' => $product,
+                    'count' => 1
+                ];
+            }
+        }
+
+        foreach ($uniqueProducts as $productData) {
+            $product = $productData['product'];
+            echo $product->toString() . ' count: ' . $productData['count'];
+        }
+
+        echo memory_get_peak_usage();
     }
 }
