4c43ed26120c86520693a6419553d87d4f5ff4b0
Daniel Gavriloaie <dani.gavriloaie@gmail.com>
2025-03-03 20:51:35 +0000

    add v flag for performance debugging

--- parser.php
+++ parser.php
@@ -4,7 +4,7 @@ use DaniGavriloaie\SupplierProductListProcessor\Services\ProductProcessor;
 
 require 'vendor/autoload.php';
 
-$options = getopt('', ['file:', 'unique-combinations:']);
+$options = getopt('v', ['file:', 'unique-combinations:']);
 
 if (empty($options['file']) || empty($options['unique-combinations'])) {
     die("Usage: php parser.php --file <file> --unique-combinations <output_file>\n");
@@ -15,4 +15,8 @@ try {
     $productProcessor->process($options['file'], $options['unique-combinations']);
 } catch (Exception $exception) {
     echo 'An error occurred: ' . $exception->getMessage();
+}
+
+if (isset($options['v'])) {
+    echo 'Peak Memory Usage: ' . (memory_get_peak_usage(true) / 1024 / 1024) . " MB\n";
 }
