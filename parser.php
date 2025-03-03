48ebf96c9169c4fb8dc5d26bda86181b4ef79cff
Daniel Gavriloaie <dani.gavriloaie@gmail.com>
2025-03-03 17:39:13 +0000

    add basic error handling

--- parser.php
+++ parser.php
@@ -10,5 +10,9 @@ if (empty($options['file']) || empty($options['unique-combinations'])) {
     die("Usage: php parser.php --file <file> --unique-combinations <output_file>\n");
 }
 
-$productProcessor = new ProductProcessor();
-$productProcessor->process($options['file'], $options['unique-combinations']);
+try {
+    $productProcessor = new ProductProcessor();
+    $productProcessor->process($options['file'], $options['unique-combinations']);
+} catch (Exception $exception) {
+    echo 'An error occurred: ' . $exception->getMessage();
+}
